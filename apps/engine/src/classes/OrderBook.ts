af73b76baa5decee4079a5fc79431b877d430a20
AdarshVMore <skullcrushermore@gmail.com>
2026-05-29 00:23:49 +0530

    optimised the code into 2 mothods to remaining Qty and add order

--- apps/engine/src/classes/OrderBook.ts
+++ apps/engine/src/classes/OrderBook.ts
@@ -1,5 +1,12 @@
 import { OrderedMap, LinkList } from "js-sdsl";
-import type { marketType, Order, OrderBooks, SingleOrderBook, User, positionType } from "@shared-types";
+import type {
+  marketType,
+  Order,
+  OrderBooks,
+  SingleOrderBook,
+  User,
+  positionType,
+} from "@shared-types";
 
 export class OrderBook {
   private orderBooks: OrderBooks = {};
@@ -17,7 +24,7 @@ export class OrderBook {
         bids: new OrderedMap<number, LinkList<Order>>(),
         lastTradedPrice: 90,
         indexPrice: 0,
-      }; 
+      };
     }
   }
 
@@ -26,54 +33,77 @@ export class OrderBook {
   }
 
   addLimitOrder(data: Order) {
-    let book = this.orderBooks[data.marketId]
-    if(!book){
-      throw new Error(`book with marketId ${data.marketId} does not exist`)
+    let book = this.orderBooks[data.marketId];
+    if (!book) {
+      throw new Error(`book with marketId ${data.marketId} does not exist`);
     }
-    let bestPrice = this.getBestPrice(data.positionType, book)
+    let bestPrice = this.getBestPrice(data.positionType, book);
 
-    if(!bestPrice){
-      throw new Error("best ptice does not exits")
+    if (!bestPrice) {
+      throw new Error("best ptice does not exits");
     }
 
     if (data.positionType === "LONG") {
-      if(bestPrice <= data.price){
-        const iterator = this.orderBooks[data.marketId]?.asks.find(bestPrice)
-        const queue = iterator?.pointer[1]
-        if(!queue){
-          // this.orderBooks[data.marketId]?.asks.eraseElementByKey(bestPrice)
-          throw new Error("there is no queue for this price")
+      if (bestPrice <= data.price) {
+        while (data.remainingQty <= 0) {
+          this.updateRemainingQty(data, bestPrice);
         }
-        const inQueueQty = queue.front()
-        if(!inQueueQty){
-          throw new Error("there is no .front() in the queue")
-        }
-        const tradeQty = Math.min(data.qty, inQueueQty?.remainingQty)
-        data.remainingQty -= tradeQty
-        inQueueQty.remainingQty -= tradeQty
+      } else {
       }
     } else {
-
     }
   }
 
   addMarketOrder(data: Order) {
     if (data.positionType === "LONG") {
-
     } else {
-
     }
   }
 
-  getBestPrice(positionType:positionType, orderBook:SingleOrderBook) {
-    if(positionType === "LONG"){
-      return orderBook.asks.front()?.[0]
+  getBestPrice(positionType: positionType, orderBook: SingleOrderBook) {
+    if (positionType === "LONG") {
+      return orderBook.asks.front()?.[0];
     } else {
-      return orderBook.bids.back()?.[0]
+      return orderBook.bids.back()?.[0];
     }
+  }
+
+  updateRemainingQty(data: Order, bestPrice: number) {
+    const iterator = this.orderBooks[data.marketId]?.asks.find(bestPrice);
+    const queue = iterator?.pointer[1];
+    if (!queue) {
+      throw new Error("there is no queue for this price");
+    }
+    const inQueueQty = queue.front();
+    if (!inQueueQty) {
+      throw new Error("there is no .front() in the queue");
+    }
+    const tradeQty = Math.min(data.qty, inQueueQty?.remainingQty);
+    data.remainingQty -= tradeQty;
+    inQueueQty.remainingQty -= tradeQty;
+    return tradeQty;
   }
 
   removeFilledOrder() {}
+
+  addToBook(data:Order){
+    if(data.positionType === "LONG"){
+      const book = this.orderBooks[data.marketId]
+      const side = data.positionType === "LONG" ? book?.bids : book?.asks
+      const list = side?.find(data.price)
+      if(!side){
+        throw new Error("side does not exists")
+      }
+      if(!list?.equals(side?.end())){
+        const queue = list?.pointer[1]
+        queue?.pushBack(data)
+      } else {
+        const newQueue = new LinkList<Order>
+        newQueue.pushBack(data)
+        this.orderBooks[data.marketId]?.bids.setElement(data.price, newQueue)
+      }
+    }
+  }
 
   deleteOrder() {}
 }
