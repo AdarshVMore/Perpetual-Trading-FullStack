6f12cdd95b7fce05acb038e03bb80c4f40954ed7
AdarshVMore <skullcrushermore@gmail.com>
2026-06-16 20:25:30 +0530

    integrated binance websocket and connected it via redis stream

---
+++ apps/binance-ws/src/index.ts
@@ -0,0 +1,20 @@
+import { redis } from "bun";
+import WebSocket, { type RawData } from "ws";
+
+const ws = new WebSocket(
+  "wss://stream.binance.com:9443/ws/btcusdt@trade/ethusdt@trade/solusdt@trade"
+);
+
+ws.on("open", () => {
+  console.log("CONNECTED");
+});
+
+ws.on("message", (data:any) => {
+  const parsed = JSON.parse(data.toString())
+  redis.publish("binance-markprices", JSON.stringify(parsed))
+  redis.set(`mark:${parsed.s}`, parsed.p)
+  console.log("publishing this data =====> ", JSON.stringify(parsed))
+  console.log("setting this data to redis", `mark:${parsed.s}`, parsed.p)
+});
+
+ws.on("error", console.error);
