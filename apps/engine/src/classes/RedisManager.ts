94ee013004ecdb5b21e4f30bff80a80353007b9c
AdarshVMore <skullcrushermore@gmail.com>
2026-06-20 03:26:29 +0530

    some changes in dbpoller and rediamanager to fix some bugs found in tests

--- apps/engine/src/classes/RedisManager.ts
+++ apps/engine/src/classes/RedisManager.ts
@@ -1,36 +1,40 @@
 import { createRedisConnection } from "@redis-client";
 import type { RedisClientType } from "redis";
 import type { EngineEvents } from "@shared-types/src";
+
 export class RedisManager {
-  private redisClient?: RedisClientType | null;
+  private subscriberClient?: RedisClientType | null;
+  private streamClient?: RedisClientType | null;
   public publisherClient?: RedisClientType | null;
 
   async connect() {
     console.log("trying to connect engine redis client");
-    this.redisClient = await createRedisConnection();
+    this.streamClient = await createRedisConnection();
 
-    if (!this.redisClient) {
+    if (!this.streamClient) {
       throw new Error("Redis client failed to connect");
     }
 
-    this.publisherClient = this.redisClient.duplicate() as RedisClientType;
+    this.subscriberClient = this.streamClient.duplicate() as RedisClientType;
+    await this.subscriberClient.connect();
+
+    this.publisherClient = this.streamClient.duplicate() as RedisClientType;
     await this.publisherClient.connect();
 
-    console.log("engine publisher redis client connected");
+    console.log("engine redis clients connected");
   }
 
   async listenToBinanceWS(callBack:((data: {marketId:string, indexPrice:number})=> void)):Promise<void> {
-    if (!this.redisClient) {
-      throw new Error("Redis client is not connected");
+    if (!this.subscriberClient) {
+      throw new Error("Redis subscriber client is not connected");
     }
 
     console.log("trying to listen for engine stream messages");
 
-    await this.redisClient.subscribe("binance-markprices", (data: string) => {
+    await this.subscriberClient.subscribe("binance-markprices", (data: string) => {
       const parsed = JSON.parse(data);
       callBack({ marketId: parsed.s, indexPrice: Number(parsed.p) })
     });
-    return 
   }
 
 
@@ -38,20 +42,19 @@ export class RedisManager {
     if (!this.publisherClient) {
       throw new Error("Redis publisher client is not connected");
     }
-    // we are sending this to ws server => we need to send depthUpdates | tradeUpdates | positionUpdates | tickerUpdates
     await this.publisherClient.publish(channel, JSON.stringify(data));
   }
 
   getPublisherClient() {
     if (!this.publisherClient) {
-      throw new Error("");
+      throw new Error("Publisher client is not connected");
     }
 
     return this.publisherClient;
   }
 
   async readFromBackendServer(){
-    const data = await this.redisClient?.xRead([{key: "send-to-engine", id: "$" }], {BLOCK: 0})
+    const data = await this.streamClient?.xRead([{key: "send-to-engine", id: "$" }], {BLOCK: 0})
     return data
   }
   
