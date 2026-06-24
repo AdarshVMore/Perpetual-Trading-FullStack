134511d2edc9960923dc85fbc031d8d56fe71466
AdarshVMore <skullcrushermore@gmail.com>
2026-06-25 00:53:38 +0530

    some better typesaftey for zod and types

--- packages/shared-types/src/zod/zod.validation.ts
+++ packages/shared-types/src/zod/zod.validation.ts
@@ -28,14 +28,13 @@ export const getFillsSchema = z.object({
 })
 
 export const cancleOrdersSchema = z.object({
-  // type:  z.enum(["create-order", "cancle-order", "get-order", "create-market"]),
   userId: z.string(),
   marketId: z.string(),
   price: z.number(),
-  qty: z.number().positive(),
-  leverage: z.number().min(1).max(100),
-  orderType: z.enum(["MARKET", "LIMIT"]),
-  positionType: z.enum(["LONG", "SHORT"])
+  positionType: z.enum(["LONG", "SHORT"]),
+  qty: z.number().positive().optional().default(0),
+  leverage: z.number().min(1).max(100).optional().default(1),
+  orderType: z.enum(["MARKET", "LIMIT"]).optional().default("LIMIT"),
 })
 
 export const createMarketSchema = z.object({
